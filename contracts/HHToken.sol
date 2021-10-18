c3e80aeb534e587c3a3912dea771cea3aff30e7c
assassin990123 <assassin990123@gmail.com>
2021-10-18 04:22:48 +0300

    feat: add ERC20 token

---
+++ contracts/HHToken.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+ 
+//Safe Math Interface
+ 
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+ 
+//ERC Token Standard #20 Interface
+ 
+abstract contract ERC20Interface {
+    function totalSupply() public virtual view returns (uint);
+    function balanceOf(address tokenOwner) public virtual view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public virtual view returns (uint remaining);
+    function transfer(address to, uint tokens) public virtual returns (bool success);
+    function approve(address spender, uint tokens) public virtual returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public virtual returns (bool success);
+ 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+ 
+ 
+//Contract function to receive approval and execute function in one call
+ 
+abstract contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) virtual public;
+}
+ 
+//Actual token contract
+ 
+contract HSCToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() {
+        symbol = "HHT";
+        name = "HSC-Hardhat-Token";
+        decimals = 6;
+        _totalSupply = 1000000000000;
+        balances[0xE7AFE8f22f66C4a7bF56ADe8285973b0A17D960a] = _totalSupply;
+        emit Transfer(address(0), 0xE7AFE8f22f66C4a7bF56ADe8285973b0A17D960a, _totalSupply);
+    }
+
+    function totalSupply() override public view returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+ 
+    function balanceOf(address tokenOwner) override public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+ 
+    function transfer(address to, uint tokens) override public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+ 
+    function approve(address spender, uint tokens) override public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+ 
+    function transferFrom(address from, address to, uint tokens) override public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+ 
+    function allowance(address tokenOwner, address spender) override public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+ 
+    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
+        return true;
+    }
+ 
+    fallback () external payable {
+        revert();
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,7 +19,7 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("Token");
+  const Token = await ethers.getContractFactory("HHToken");
   const token = await Token.deploy();
   await token.deployed();
 
@@ -42,7 +42,7 @@ function saveFrontendFiles(token) {
     JSON.stringify({ Token: token.address }, undefined, 2)
   );
 
-  const TokenArtifact = artifacts.readArtifactSync("Token");
+  const TokenArtifact = artifacts.readArtifactSync("HHToken");
 
   fs.writeFileSync(
     contractsDir + "/Token.json",
